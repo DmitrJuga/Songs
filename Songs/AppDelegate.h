93ce5fe50c635a57ec7fc7b2e3035b375e933095
DmitrJuga <dmitrjuga@gmail.com>
2015-07-06 15:51:57 +0300

    Initial Commit

---
+++ Songs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Songs
+//
+//  Created by DmitrJuga on 06.07.15.
+//  Copyright (c) 2015 Dmitriy Dolotenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Songs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Songs
+//
+//  Created by DmitrJuga on 06.07.15.
+//  Copyright (c) 2015 Dmitriy Dolotenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
